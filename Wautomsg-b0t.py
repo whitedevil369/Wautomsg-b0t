35a7e0fdbc31c729ebbe8cbb2f5588afabb30645
Poornesh Adhithya <56251750+whitedevil369@users.noreply.github.com>
2020-09-10 02:37:51 +0530

    Update Wautomsg-b0t.py

--- Wautomsg-b0t.py
+++ Wautomsg-b0t.py
@@ -38,16 +38,16 @@ def home():
 	print ("Name: Wautomsg-b0t")
 	print ("version: 1.0")
 	print ("@whitedevil369")
-	print f1.renderText('Wautomsg-b0t')
-	print f2.renderText("Made by Poornesh Adhithya")	
+	print (f1.renderText('Wautomsg-b0t'))
+	print (f2.renderText("Made by Poornesh Adhithya"))	
 	print ("\nWELCOME TO Wautomsg-b0t\nWautomsg-b0t is a WHATSAPP MASS/BULK MESSAGE SENDER\n(WITHOUT SAVING THE PHONE NUMBERS IN CONTACTS)\n")
-	raw_input("\nPRESS ENTER TO CONTINUE.\n\nb0t:~#> ")
+	input("\nPRESS ENTER TO CONTINUE.\n\nb0t:~#> ")
 
 home()
 print ("\nENTER SENDER PHONE NUMBER: ")
 print ("format: <country_code><phone_number> (without + or 00 or spaces)")
 print ("Eg. 911234567890\nWhere 91 is the Counry Code and 1234567890 is phone number")
-sender_ph = raw_input("\nb0t:~#> ")
+sender_ph = input("\nb0t:~#> ")
 print("\n")
 
 if not sender_ph.isdigit() or '\n' in sender_ph:
@@ -57,7 +57,7 @@ if not sender_ph.isdigit() or '\n' in sender_ph:
 
 print ("\nENTER THE SOURCE LOCATION OF THE PHONE NUMBERS LIST: ")
 print ("Eg. /root/Desktop/contacts.txt\nIf it exists in the same dirctory of the file then,\nEg. contacts.txt")
-src = raw_input("\nb0t:~#> ")
+src = input("\nb0t:~#> ")
 print("\n")
 
 try:
@@ -68,6 +68,7 @@ except:
 	print("FILE DOESN'T EXIST!")
 	exit_timer()		
 	exit()
+
 
 if not src.endswith('.txt'):
 	print("ONLY .txt FILE FORMAT IS ACCEPTED")
@@ -85,15 +86,16 @@ for p in phone:
 		exit_timer()
 		exit()
 
+
 print ("\nENTER YOUR MODE OF MESSAGING (cli/file): ")
 print ("cli to enter Message in the terminal.\nfile to import the location of the file")
-c = raw_input("\nb0t:~#> ")
+c = input("\nb0t:~#> ")
 print("\n")
 
 if c=='file':
 	print ("\nENTER THE SOURCE LOCATION OF THE MESSAGE: ")
 	print ("Eg. /root/Desktop/samplemsg.txt\nIf it exists in the same dirctory of the file then,\nEg. samplemsg.txt")
-	loc = raw_input("\nb0t:~#> ")
+	loc = input("\nb0t:~#> ")
 	if not loc.endswith('.txt'):
 		print("ONLY .txt FILE FORMAT IS ACCEPTED")
 		exit_timer()		
@@ -120,37 +122,32 @@ else:
 	exit_timer()
 	exit()
 
-#<------------------------->
-
-#positions
-pos_x = 1229	# value of x in positionfinder.py
-pos_y = 781	# value of y in positionfinder.py
-#THIS IS FOR 1600x900 RESOLUTION, VALUES MAY CHANGE ACCORDING TO YOUR PCs RESOLUTION. (use positionfinder.py tool to find your mouse position)
-#Change These Values Before Executing
-
-#<-------------------------->
-
-raw_input("\nPRESS Enter To Start The b0t and then Switch to WhatsApp Web.\n\nb0t:~#> ")
+input("\nPRESS Enter To Start The b0t and then Switch to WhatsApp Web.\n\nb0t:~#> ")
 print("\nYou have 5 seconds to switch to WhatsApp Web.....")
 time.sleep(5)
 print("Switch to WhatsApp Web, Your running out of time...\n")
 alert()
 pyautogui.typewrite('Wa.me/'+sender_ph)
 pyautogui.press('enter')
-position = pos_x,pos_y
+position = 1229,781
 pyautogui.click(position)
 pyautogui.typewrite('Test Message')
 pyautogui.press('enter')	
 pyautogui.press('esc')
 time.sleep(2)
 pyautogui.press('esc')
-
+#positions
+pos_x = 1229	# value of x in positionfinder.py
+pos_y = 781	# value of y in positionfinder.py
+#THIS IS FOR 1600x900 RESOLUTION, VALUES MAY CHANGE ACCORDING TO YOUR PCs RESOLUTION. (use positionfinder.py tool to find your mouse position)
+#Change These Values Before Executing
 i=0
 try:
 	for number in phone:
 		time.sleep(2)
 		pyautogui.typewrite('Wa.me/'+number)
 		pyautogui.press('enter')
+		position = pos_x,pos_y	
 		pyautogui.click(position)
 		time.sleep(2)
 		#pyautogui.typewrite('Write Something')
